1a5efc76eb63518934cb40d1f4e3a2997e49462e
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-02-16 12:41:19 +0300

    Add MainWindowViewModel

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Maelstrom.ViewModels.Base;
+
+namespace Maelstrom.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "title_plchldr";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
