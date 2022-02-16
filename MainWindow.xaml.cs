af89d48125c67925ebe15e3f539696d5bf1ed235
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-02-16 11:57:49 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Maelstrom
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
