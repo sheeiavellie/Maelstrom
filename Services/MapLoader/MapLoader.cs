e3ac444246b58b92489a73cff19550f70814ed06
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-02-20 20:04:02 +0300

    Add MapLoader

---
+++ Services/MapLoader/MapLoader.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Maelstrom.Services.MapLoader
+{
+    internal class MapLoader
+    {
+        public static void ReadMap(string jsonMapFileIn)
+        {
+            dynamic jsonMapFile = JsonConvert.DeserializeObject(File.ReadAllText(jsonMapFileIn));
+        }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,30 +1,50 @@
 ﻿using Maelstrom.Infrastructure.Commands;
-using Maelstrom.Models.Objects.Envirnoment;
 using Maelstrom.Models.Objects.GameObjects;
-using Maelstrom.Models.Objects.GameObjects.CharacterClasses;
 using Maelstrom.ViewModels.Base;
-using System.Windows;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Maelstrom.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        private string _Title = "Maelstorm project";
+        #region Title
+        public string _Title = "Maelstorm project";
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
         }
-        public ICommand MoveRight { get; set; }
-        private bool CanMoveRightExecute(object p) => true;
-        private void OnMoveRightExecuted(object p)
+        #endregion
+
+        #region GameObjects
+        private ObservableCollection<GameObject> _GameObjects;
+        public ObservableCollection<GameObject> GameObjects
         {
-               
+            get => _GameObjects;
+            set => Set(ref _GameObjects, value);
         }
+        #endregion
+
+
+
+        /*public ICommand TestCommand { get; }
+        private void OnTestCommandExecuted(object p)
+        {
+        }
+        private bool CanTestCommandExecute(object p) => true;*/
+
         public MainWindowViewModel()
         {
-            MoveRight = new RelayCommand(OnMoveRightExecuted, CanMoveRightExecute);
+            //TestCommand = new RelayCommand(OnTestCommandExecuted, CanTestCommandExecute);
+
+            var player = new Character(1, 1);
+            player.Name = "bob";
+
+            GameObjects = new ObservableCollection<GameObject>();
+
+            GameObjects.Add(player);
         }
     }
 }
