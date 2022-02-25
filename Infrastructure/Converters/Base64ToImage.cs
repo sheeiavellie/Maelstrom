314e4be4700fe99321447b7eedaaa7bf020a081b
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-02-25 18:24:34 +0300

    Add base Converter class, implement Base64ToImage converter

---
+++ Infrastructure/Converters/Base64ToImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Maelstrom.Infrastructure.Converters
+{
+    internal class Base64ToImage : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+
+            if (s == null)
+                return null;
+
+            BitmapImage bi = new BitmapImage();
+
+            bi.BeginInit();
+            bi.StreamSource = new MemoryStream(System.Convert.FromBase64String(s));
+            bi.EndInit();
+
+            return bi;
+        }
+
+        public override object ConvertBack(object v, Type t, object p, CultureInfo c) => throw new NotImplementedException("Not Implemented yet");
+    }
+}
---
+++ Infrastructure/Converters/Converter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Maelstrom.Infrastructure.Converters
+{
+    internal abstract class Converter : IValueConverter
+    {
+        public abstract object Convert(object v, Type t, object p, CultureInfo c);
+
+        public virtual object ConvertBack(object v, Type t, object p, CultureInfo c) => throw new NotImplementedException("Not Implemented yet");
+    }
+}
