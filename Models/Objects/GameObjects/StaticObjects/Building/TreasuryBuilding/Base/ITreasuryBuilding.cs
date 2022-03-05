fe2041474d5f84951c325d56212d58c84e3bb2b9
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-03-05 16:57:47 +0300

    Add battle window to implement battles

---
+++ Models/Objects/GameObjects/StaticObjects/Building/TreasuryBuilding/Base/ITreasuryBuilding.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maelstrom.Models.Objects.GameObjects.StaticObjects.Building.TreasuryBuilding.Base
+{
+    interface ITreasuryBuilding
+    {
+    }
+}
---
+++ Models/Objects/GameObjects/StaticObjects/Building/TreasuryBuilding/Base/TreasuryBuilding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maelstrom.Models.Objects.GameObjects.StaticObjects.Building.TreasuryBuilding.Base
+{
+    internal abstract class TreasuryBuilding : Building, ITreasuryBuilding
+    {
+        
+    }
+}
