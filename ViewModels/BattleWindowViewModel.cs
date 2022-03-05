8c68cce5701e2b86ea2a436763fee9909499cfbd
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-03-05 18:40:33 +0300

    Create basic test UI for battle window

--- ViewModels/BattleWindowViewModel.cs
+++ ViewModels/BattleWindowViewModel.cs
@@ -34,14 +34,14 @@ namespace Maelstrom.ViewModels
         public ICommand PlayerMoveRightCommand { get; }
         private bool CanPlayerMoveRightCommandExecute(object p)
         {
-            if (PColumn < 4)
+            if (PColumn < 9)
                 return true;
             return false;
         }
         private void OnPlayerMoveRightCommandExecuted(object p)
         {
-            
             PColumn += 1;
+            LogValue = PRow.ToString() + "; " + PColumn.ToString();
         }
         #endregion
 
@@ -56,6 +56,7 @@ namespace Maelstrom.ViewModels
         private void OnPlayerMoveLeftCommandExecuted(object p)
         {
             PColumn -= 1;
+            LogValue = PRow.ToString() + "; " + PColumn.ToString();
         }
         #endregion
 
@@ -69,8 +70,8 @@ namespace Maelstrom.ViewModels
         }
         private void OnPlayerMoveUpCommandExecuted(object p)
         {
-           
             PRow -= 1;
+            LogValue = PRow.ToString() + "; " + PColumn.ToString();
         }
         #endregion
 
@@ -78,7 +79,7 @@ namespace Maelstrom.ViewModels
         public ICommand PlayerMoveDownCommand { get; }
         private bool CanPlayerMoveDownCommandExecute(object p)
         {
-            if (PRow < 5)
+            if (PRow < 9)
                 return true;
             return false;
         }
@@ -86,16 +87,28 @@ namespace Maelstrom.ViewModels
         {
             
             PRow += 1;
+            LogValue = PRow.ToString() + "; " + PColumn.ToString();
         }
         #endregion 
 
         #endregion
 
 
+
+        private string _LogValue;
+        public string LogValue
+        {
+            get => _LogValue;
+            set => Set(ref _LogValue, value);
+        }
+
+
         public BattleWindowViewModel()
         {
             PColumn = 1;
             PRow = 1;
+
+            LogValue = "Initialized";
 
             PlayerMoveRightCommand = new RelayCommand(OnPlayerMoveRightCommandExecuted, CanPlayerMoveRightCommandExecute);
             PlayerMoveLeftCommand = new RelayCommand(OnPlayerMoveLeftCommandExecuted, CanPlayerMoveLeftCommandExecute);
