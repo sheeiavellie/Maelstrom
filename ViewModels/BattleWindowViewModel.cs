adffcb2b608dd107cddeb810a7c04a5a7aed1b58
sheeiavellie <98523826+sheeiavellie@users.noreply.github.com>
2022-03-05 18:15:40 +0300

    Add ViewModel for battle window

---
+++ ViewModels/BattleWindowViewModel.cs
@@ -0,0 +1,107 @@
+﻿using Maelstrom.Infrastructure.Commands;
+using Maelstrom.ViewModels.Base;
+using System.Windows.Input;
+
+namespace Maelstrom.ViewModels
+{
+    internal class BattleWindowViewModel : ViewModel
+    {
+        #region Player Position
+
+        #region Player Row
+        private int _PRow;
+        public int PRow
+        {
+            get => _PRow;
+            set => Set(ref _PRow, value);
+        }
+        #endregion
+
+        #region Player Column
+        private int _PColumn;
+        public int PColumn
+        {
+            get => _PColumn;
+            set => Set(ref _PColumn, value);
+        }
+        #endregion 
+
+        #endregion
+
+        #region Player Controls
+
+        #region Right
+        public ICommand PlayerMoveRightCommand { get; }
+        private bool CanPlayerMoveRightCommandExecute(object p)
+        {
+            if (PColumn < 4)
+                return true;
+            return false;
+        }
+        private void OnPlayerMoveRightCommandExecuted(object p)
+        {
+            
+            PColumn += 1;
+        }
+        #endregion
+
+        #region Left
+        public ICommand PlayerMoveLeftCommand { get; }
+        private bool CanPlayerMoveLeftCommandExecute(object p)
+        {
+            if (PColumn > 0)
+                return true;
+            return false;
+        }
+        private void OnPlayerMoveLeftCommandExecuted(object p)
+        {
+            PColumn -= 1;
+        }
+        #endregion
+
+        #region Up
+        public ICommand PlayerMoveUpCommand { get; }
+        private bool CanPlayerMoveUpCommandExecute(object p)
+        {
+            if (PRow > 0 )
+                return true;
+            return false;
+        }
+        private void OnPlayerMoveUpCommandExecuted(object p)
+        {
+           
+            PRow -= 1;
+        }
+        #endregion
+
+        #region Down
+        public ICommand PlayerMoveDownCommand { get; }
+        private bool CanPlayerMoveDownCommandExecute(object p)
+        {
+            if (PRow < 5)
+                return true;
+            return false;
+        }
+        private void OnPlayerMoveDownCommandExecuted(object p)
+        {
+            
+            PRow += 1;
+        }
+        #endregion 
+
+        #endregion
+
+
+        public BattleWindowViewModel()
+        {
+            PColumn = 1;
+            PRow = 1;
+
+            PlayerMoveRightCommand = new RelayCommand(OnPlayerMoveRightCommandExecuted, CanPlayerMoveRightCommandExecute);
+            PlayerMoveLeftCommand = new RelayCommand(OnPlayerMoveLeftCommandExecuted, CanPlayerMoveLeftCommandExecute);
+            PlayerMoveUpCommand = new RelayCommand(OnPlayerMoveUpCommandExecuted, CanPlayerMoveUpCommandExecute);
+            PlayerMoveDownCommand = new RelayCommand(OnPlayerMoveDownCommandExecuted, CanPlayerMoveDownCommandExecute);
+
+        }
+    }
+}
